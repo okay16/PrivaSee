730ba2bb5a125cc9265f075679ed605132e725df
Ojasvi Khanna <ojasvikhanna@gmail.com>
2020-06-22 13:21:08 +0530

    Add files via upload

---
+++ privasee/popup.js
@@ -0,0 +1,6 @@
+https://api.smmry.com/&SM_API_KEY=D97274EBA9&SM_URL=https://policies.google.com/privacy?fg=1&SM_LENGTH=1
+
+"https://api.smmry.com/"
+"&SM_API_KEY=D97274EBA9"
+"&SM_URL=https://policies.google.com/privacy?fg=1"
+"&SM_LENGTH=1"
